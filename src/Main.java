f8cf913889dce257d00d24a19bfeb037438bdacc
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-11-04 18:38:30 +0300

    Task-2

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,30 @@ public class Main {
         arrayArbitrary[0] = true;
         arrayArbitrary[2] = true;
         System.out.println(Arrays.toString(arrayArbitrary));
+
+        // Task-2
+        for (byte i = 0; i < arrayInt.length; i++) {
+            if (i == arrayInt.length - 1) {
+                System.out.println(arrayInt[i]);
+                break;
+            }
+            System.out.print(arrayInt[i] + ", ");
+        }
+
+        for (byte i = 0; i < arrayFloat.length; i++) {
+            if (i == arrayFloat.length - 1) {
+                System.out.println(arrayFloat[i]);
+                break;
+            }
+            System.out.print(arrayFloat[i] + ", ");
+        }
+
+        for (byte i = 0; i < arrayArbitrary.length; i++) {
+            if (i == arrayArbitrary.length - 1) {
+                System.out.println(arrayArbitrary[i]);
+                break;
+            }
+            System.out.print(arrayArbitrary[i] + ", ");
+        }
     }
 }
