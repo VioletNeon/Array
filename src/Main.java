7501aef9622447cf93bd34005ef021b170583edf
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-11-04 18:31:21 +0300

    Task-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task-1
+        int[] arrayInt = new int[3];
+
+        for (byte i = 0; i < arrayInt.length; i++) {
+            arrayInt[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(arrayInt));
+
+        float[] arrayFloat = {1.57F, 7.654F, 9.986F};
+        System.out.println(Arrays.toString(arrayFloat));
+
+        boolean[] arrayArbitrary = new boolean[3];
+        arrayArbitrary[0] = true;
+        arrayArbitrary[2] = true;
+        System.out.println(Arrays.toString(arrayArbitrary));
     }
 }
